e2a26e39c41f7f256a5a77ed7c12118003649ac9
Michael Williams <michael.williams@enspiral.com>
2019-02-13 18:53:46 +1300

    add dat share loop to release

--- release.sh
+++ release.sh
@@ -12,4 +12,10 @@ cd releases/${TODAY}
 
 npx dat create --title "PeachPi image ${TODAY}" --description "Debian image for a Raspberry Pi based PeachCloud"
 
-npx dat share
+# do a dat share loop because stalls after some time?
+trap exit EXIT
+while true
+do
+  timeout 360s npx dat share
+  sleep 10
+done
